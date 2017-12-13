08bc470d56eee1dc3c2688661f0f33a6a4e4b8b5
zhumingzhen <itdream53@gmail.com>
2017-12-13 16:16:23 +0800

    debugr

--- app/Http/Controllers/OfficialAccount/ServerController.php
+++ app/Http/Controllers/OfficialAccount/ServerController.php
@@ -12,7 +12,7 @@ class ServerController extends Controller
     {
         $config = [
           'app_id' => 'wxe6bd61546e337818',
-          'seccret' => '5950e505a3b38df6225d02e4f6fcc91a',
+          'secret' => '5950e505a3b38df6225d02e4f6fcc91a',
           'aes_key' => 'tOihYdEo0SRv0b6RrkxjxIRjxKM8uCe5xrEevtCDhJf',
           'token' => 'yinxce'
         ];
