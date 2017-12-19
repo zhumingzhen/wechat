14ec65ceadabab325d0a94cd9996d303a9c8572a
zhumingzhen <itdream53@gmail.com>
2017-12-20 00:39:23 +0800

    menu

--- app/Http/Controllers/OfficialAccount/ServerController.php
+++ app/Http/Controllers/OfficialAccount/ServerController.php
@@ -24,13 +24,64 @@ class ServerController extends Controller
 
         $app = Factory::officialAccount($config);
 
-        $app->server->push(function ($message) use ($app){
-            $user = $app->user->get($message['FromUserName']);
-            return '你好!'.$user['nickname'].'你说了一句'.$message['Content'];
-        });
+//        $app->server->push(function ($message) use ($app){
+//            $user = $app->user->get($message['FromUserName']);
+//            return '你好!'.$user['nickname'].'你说了一句'.$message['Content'];
+//        });
+//
+//        $response = $app->server->serve();
 
-        $response = $app->server->serve();
+        $menu = '{
+                    "button": [
+                        {
+                            "name": "扫码", 
+                            "sub_button": [
+                                {
+                                    "type": "scancode_waitmsg", 
+                                    "name": "扫码带提示", 
+                                    "key": "rselfmenu_0_0", 
+                                    "sub_button": [ ]
+                                }, 
+                                {
+                                    "type": "scancode_push", 
+                                    "name": "扫码推事件", 
+                                    "key": "rselfmenu_0_1", 
+                                    "sub_button": [ ]
+                                }
+                            ]
+                        }, 
+                        {
+                            "name": "发图", 
+                            "sub_button": [
+                                {
+                                    "type": "pic_sysphoto", 
+                                    "name": "系统拍照发图", 
+                                    "key": "rselfmenu_1_0", 
+                                   "sub_button": [ ]
+                                 }, 
+                                {
+                                    "type": "pic_photo_or_album", 
+                                    "name": "拍照或者相册发图", 
+                                    "key": "rselfmenu_1_1", 
+                                    "sub_button": [ ]
+                                }, 
+                                {
+                                    "type": "pic_weixin", 
+                                    "name": "微信相册发图", 
+                                    "key": "rselfmenu_1_2", 
+                                    "sub_button": [ ]
+                                }
+                            ]
+                        }, 
+                        {
+                            "name": "发送位置", 
+                            "type": "location_select", 
+                            "key": "rselfmenu_2_0"
+                        }
+                    ]
+                }';
 
+        $response = $app->menu->create($menu);
         return $response;
     }
 }
