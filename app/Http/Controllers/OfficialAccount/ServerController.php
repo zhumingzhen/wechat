a77e1e5afe1af9ef0fcf2d94e1e6cf821f3dd4a1
zhumingzhen <itdream53@gmail.com>
2017-12-20 00:55:32 +0800

    menu

--- app/Http/Controllers/OfficialAccount/ServerController.php
+++ app/Http/Controllers/OfficialAccount/ServerController.php
@@ -31,84 +31,56 @@ class ServerController extends Controller
 //
 //        $response = $app->server->serve();
 
-        /*$menu = '[
+        $menu = [
                     [
-                        "name": "扫码", 
-                        "sub_button": [
+                        "name"=>"扫码",
+                        "sub_button"=> [
                             [
-                                "type": "scancode_waitmsg", 
-                                "name": "扫码带提示", 
-                                "key": "rselfmenu_0_0", 
-                                "sub_button": [ ]
+                                "type"=> "scancode_waitmsg",
+                                "name"=> "扫码带提示",
+                                "key"=> "rselfmenu_0_0",
+                                "sub_button"=> [ ]
                             ], 
                             [
-                                "type": "scancode_push", 
-                                "name": "扫码推事件", 
-                                "key": "rselfmenu_0_1", 
-                                "sub_button": [ ]
+                                "type"=> "scancode_push",
+                                "name"=> "扫码推事件",
+                                "key"=> "rselfmenu_0_1",
+                                "sub_button"=> [ ]
                             ]
                         ]
                     ], 
                     [
-                        "name": "发图", 
-                        "sub_button": [
+                        "name"=> "发图",
+                        "sub_button"=> [
                             [
-                                "type": "pic_sysphoto", 
-                                "name": "系统拍照发图", 
-                                "key": "rselfmenu_1_0", 
-                               "sub_button": [ ]
+                                "type"=> "pic_sysphoto",
+                                "name"=> "系统拍照发图",
+                                "key"=> "rselfmenu_1_0",
+                               "sub_button"=> [ ]
                              ], 
                             [
-                                "type": "pic_photo_or_album", 
-                                "name": "拍照或者相册发图", 
-                                "key": "rselfmenu_1_1", 
-                                "sub_button": [ ]
+                                "type"=> "pic_photo_or_album",
+                                "name"=> "拍照或者相册发图",
+                                "key"=> "rselfmenu_1_1",
+                                "sub_button"=> [ ]
                             ], 
                             [
-                                "type": "pic_weixin", 
-                                "name": "微信相册发图", 
-                                "key": "rselfmenu_1_2", 
-                                "sub_button": [ ]
+                                "type"=> "pic_weixin",
+                                "name"=> "微信相册发图",
+                                "key"=>"rselfmenu_1_2",
+                                "sub_button"=> [ ]
                             ]
                         ]
                     ], 
                     [
-                        "name": "发送位置", 
-                        "type": "location_select", 
-                        "key": "rselfmenu_2_0"
+                        "name"=> "发送位置",
+                        "type"=> "location_select",
+                        "key"=> "rselfmenu_2_0"
                     ]
-                ]';
+                ];
 
         $response = $app->menu->create($menu);
-        return $response;*/
-        $buttons = [
-            [
-                "type" => "click",
-                "name" => "今日歌曲",
-                "key"  => "V1001_TODAY_MUSIC"
-            ],
-            [
-                "name"       => "菜单",
-                "sub_button" => [
-                    [
-                        "type" => "view",
-                        "name" => "搜索",
-                        "url"  => "http://www.soso.com/"
-                    ],
-                    [
-                        "type" => "view",
-                        "name" => "视频",
-                        "url"  => "http://v.qq.com/"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "赞一下我们",
-                        "key" => "V1001_GOOD"
-                    ],
-                ],
-            ],
-        ];
-        $result = $app->menu->create($buttons);
-        return $result;
+        return $response;
+
     }
 }
