031c9ad16ea72addb0a0707a0f456b87b3807db4
zhumingzhen <itdream53@gmail.com>
2017-12-20 23:47:22 +0800

    de

--- app/Http/Controllers/OfficialAccount/ServerController.php
+++ app/Http/Controllers/OfficialAccount/ServerController.php
@@ -9,10 +9,10 @@ use EasyWeChat\Factory;
 
 class ServerController extends Controller
 {
-    private $app;
 
-    public function __construct()
+    public function index()
     {
+
         $config = [
             'app_id' => 'wxe6bd61546e337818',
             'secret' => 'a4c7347a2500c0e76d806f6c463dd0db',
@@ -24,13 +24,8 @@ class ServerController extends Controller
             ],
         ];
 
-        $this->app = Factory::officialAccount($config);
-    }
+        $app = Factory::officialAccount($config);
 
-    public function index()
-    {
-
-        $app = $this->app; 
         $app->server->push(function ($message) use ($app)[
             $user = $app->user->get($message['FromUserName']);
             return '你好!'.$user['nickname'].'你说了一句'.$message['Content'];
