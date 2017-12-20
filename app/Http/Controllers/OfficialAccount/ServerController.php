944193f7ead9c7e0bf9f6afb8ab04762552063c6
zhumingzhen <itdream53@gmail.com>
2017-12-20 23:49:48 +0800

    de

--- app/Http/Controllers/OfficialAccount/ServerController.php
+++ app/Http/Controllers/OfficialAccount/ServerController.php
@@ -9,10 +9,8 @@ use EasyWeChat\Factory;
 
 class ServerController extends Controller
 {
-
     public function index()
     {
-
         $config = [
             'app_id' => 'wxe6bd61546e337818',
             'secret' => 'a4c7347a2500c0e76d806f6c463dd0db',
@@ -33,63 +31,56 @@ class ServerController extends Controller
 
         $response = $app->server->serve();
 
-
-        return $response;
+//        $menu = [
+//                    [
+//                        "name"=>"扫码",
+//                        "sub_button"=> [
+//                            [
+//                                "type"=> "scancode_waitmsg",
+//                                "name"=> "扫码带提示",
+//                                "key"=> "rselfmenu_0_0",
+//                                "sub_button"=> [ ]
+//                            ],
+//                            [
+//                                "type"=> "scancode_push",
+//                                "name"=> "扫码推事件",
+//                                "key"=> "rselfmenu_0_1",
+//                                "sub_button"=> [ ]
+//                            ]
+//                        ]
+//                    ],
+//                    [
+//                        "name"=> "发图",
+//                        "sub_button"=> [
+//                            [
+//                                "type"=> "pic_sysphoto",
+//                                "name"=> "系统拍照发图",
+//                                "key"=> "rselfmenu_1_0",
+//                               "sub_button"=> [ ]
+//                             ],
+//                            [
+//                                "type"=> "pic_photo_or_album",
+//                                "name"=> "拍照或者相册发图",
+//                                "key"=> "rselfmenu_1_1",
+//                                "sub_button"=> [ ]
+//                            ],
+//                            [
+//                                "type"=> "pic_weixin",
+//                                "name"=> "微信相册发图",
+//                                "key"=>"rselfmenu_1_2",
+//                                "sub_button"=> [ ]
+//                            ]
+//                        ]
+//                    ],
+//                    [
+//                        "name"=> "发送位置",
+//                        "type"=> "location_select",
+//                        "key"=> "rselfmenu_2_0"
+//                    ]
+//                ];
+//
+//        $response = $app->menu->create($menu);
+//        return $response;
 
     }
-
-
-    /*
-
-    $menu = [
-                    [
-                        "name"=>"扫码",
-                        "sub_button"=> [
-                            [
-                                "type"=> "scancode_waitmsg",
-                                "name"=> "扫码带提示",
-                                "key"=> "rselfmenu_0_0",
-                                "sub_button"=> [ ]
-                            ],
-                            [
-                                "type"=> "scancode_push",
-                                "name"=> "扫码推事件",
-                                "key"=> "rselfmenu_0_1",
-                                "sub_button"=> [ ]
-                            ]
-                        ]
-                    ],
-                    [
-                        "name"=> "发图",
-                        "sub_button"=> [
-                            [
-                                "type"=> "pic_sysphoto",
-                                "name"=> "系统拍照发图",
-                                "key"=> "rselfmenu_1_0",
-                               "sub_button"=> [ ]
-                             ],
-                            [
-                                "type"=> "pic_photo_or_album",
-                                "name"=> "拍照或者相册发图",
-                                "key"=> "rselfmenu_1_1",
-                                "sub_button"=> [ ]
-                            ],
-                            [
-                                "type"=> "pic_weixin",
-                                "name"=> "微信相册发图",
-                                "key"=>"rselfmenu_1_2",
-                                "sub_button"=> [ ]
-                            ]
-                        ]
-                    ],
-                    [
-                        "name"=> "发送位置",
-                        "type"=> "location_select",
-                        "key"=> "rselfmenu_2_0"
-                    ]
-                ];
-
-        $response = $app->menu->create($menu);
-
-    */
 }
