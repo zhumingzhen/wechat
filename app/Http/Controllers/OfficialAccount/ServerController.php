465796f25cbc2e38fa7b8ad0c643f4d89dcab0fd
zhumingzhen <itdream53@gmail.com>
2017-12-20 23:52:45 +0800

    de

--- app/Http/Controllers/OfficialAccount/ServerController.php
+++ app/Http/Controllers/OfficialAccount/ServerController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\OfficialAccount;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use EasyWeChat\Factory;
+use EasyWeChat\Kernel\Messages;
 
 class ServerController extends Controller
 {
