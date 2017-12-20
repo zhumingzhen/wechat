c66a895a72f13c555c55008cb9184983c9681411
zhumingzhen <itdream53@gmail.com>
2017-12-21 00:38:12 +0800

    de

--- app/Http/Controllers/OfficialAccount/ServerController.php
+++ app/Http/Controllers/OfficialAccount/ServerController.php
@@ -34,7 +34,7 @@ class ServerController extends Controller
             $user = $app->user->get($message['FromUserName']);
             switch ($message['MsgType']) {
                 case 'event':
-                    if ($message['MsgType']['Event'] == 'subscribe'){
+                    if ($message['Event'] == 'subscribe'){
                         return "感谢您关注【卓锦苏州】"."\n"."微信号：zhuojinsz"."\n"."卓越锦绣，名城苏州，我们为您提供苏州本地生活指南，苏州相关信息查询，做最好的苏州微信平台。"."\n"."目前平台功能如下："."\n"."【1】 查天气，如输入：苏州天气"."\n"."【2】 查公交，如输入：苏州公交178"."\n"."【3】 翻译，如输入：翻译I love you"."\n"."【4】 苏州信息查询，如输入：苏州观前街"."\n"."更多内容，敬请期待...";
                     }
                     return '收到事件消息';
