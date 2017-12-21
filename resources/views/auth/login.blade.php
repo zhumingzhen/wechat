b28754db69ce6e027b9a2e3e1afd25b2ad2708a0
zhumingzhen <itdream53@gmail.com>
2017-12-21 17:53:27 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="../../index2.html"><b>Admin</b>LTE</a>
+        <a href="../../index2.html"><b>itdream</b>wechat</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
