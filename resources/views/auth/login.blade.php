293cdb5ec00fb2f344e27be1b0fac565b74a79b4
zhumingzhen <itdream53@gmail.com>
2017-12-21 21:48:59 +0800

    修改提交

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,20 +36,21 @@
     <div class="login-box-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
-        <form action="../../index2.html" method="post">
+        <form action="{{ route('login') }}" method="post">
+            {{ csrf_field() }}
             <div class="form-group has-feedback">
-                <input type="email" class="form-control" placeholder="Email">
+                <input type="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="Email">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Password">
+                <input type="password" name="password" class="form-control" value="{{ old('Password') }}" placeholder="Password">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
                 <div class="col-xs-8">
                     <div class="checkbox icheck">
                         <label>
-                            <input type="checkbox"> Remember Me
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
                         </label>
                     </div>
                 </div>
@@ -70,7 +71,7 @@
         </div>
         <!-- /.social-auth-links -->
 
-        <a href="#">I forgot my password</a><br>
+        <a href="{{ route('password.request') }}">I forgot my password</a><br>
         <a href="register.html" class="text-center">Register a new membership</a>
 
     </div>
