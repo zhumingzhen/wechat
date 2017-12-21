126986eee399e144f97011df726442dc87483c72
zhumingzhen <itdream53@gmail.com>
2017-12-21 12:16:05 +0800

    jump login

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,13 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::any('/server', 'OfficialAccount\ServerController@index');
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
