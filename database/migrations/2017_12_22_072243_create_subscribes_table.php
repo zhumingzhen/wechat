3033673952c54972fb70379ba95bc8e3d26a9148
zhumingzhen <itdream53@gmail.com>
2017-12-22 16:27:18 +0800

    add database

--- database/migrations/2017_12_22_072243_create_subscribes_table.php
+++ database/migrations/2017_12_22_072243_create_subscribes_table.php
@@ -19,7 +19,7 @@ class CreateSubscribesTable extends Migration
             $table->string('content', 256)->comment('内容');
             $table->tinyInteger('validity')->comment('激活状态')->default('0');
             $table->tinyInteger('default')->comment('默认状态')->default('0');
-            $table->timestamp('start_time')->comment('开始时间');
+            $table->timestamp('start_time')->comment('开始时间')->default('CURRENT_TIMESTAMP');
             $table->timestamp('end_time')->comment('结束时间')->nullable();
             $table->timestamps();
             $table->softDeletes();
