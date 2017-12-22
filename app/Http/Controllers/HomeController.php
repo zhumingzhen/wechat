4acbbb505ecac1db3b50d3b86da0eabf31d54c5a
zhumingzhen <itdream53@gmail.com>
2017-12-23 01:13:00 +0800

    Request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
     {
         $input = $request->all();
         unset($input['_token']);
-        Subscribe::updateOrCreate(['id' => 7],$input);
+        Subscribe::updateOrCreate(['id' => 7]);
 //        $Subscribe = new Subscribe;
 //        $Subscribe->title = $request->title;
 //        $Subscribe->content = $request->content;
